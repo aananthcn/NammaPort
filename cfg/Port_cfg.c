e6f02b6276e8f2c9e0f34c18e900d4cd5c4479c2
Aananth C N <c.n.aananth@gmail.com>
2022-09-14 22:06:27 +0530

    Autogenerated code for LED Port 25 conf

--- cfg/Port_cfg.c
+++ cfg/Port_cfg.c
@@ -6,38 +6,11 @@
 Port_ConfigType PortConfigs = {
 	.num_pins = PORT_NUM_OF_PINS,
 	.pin[0] = {
-		.pin_id = 65535,
-		.pin_dir = PORT_PIN_IN,
+		.pin_id = 25,
+		.pin_dir = PORT_PIN_OUT,
 		.pin_mode = PORT_PIN_MODE_DIO,
 		.pin_level = PORT_PIN_LEVEL_LOW,
 		.pin_initial_mode = PORT_PIN_MODE_DIO,
-		.pin_dir_changeable = FALSE,
-		.pin_mode_changeable = FALSE
-	},
-	.pin[1] = {
-		.pin_id = 34,
-		.pin_dir = PORT_PIN_OUT,
-		.pin_mode = PORT_PIN_MODE_DIO_WDG,
-		.pin_level = PORT_PIN_LEVEL_HIGH,
-		.pin_initial_mode = PORT_PIN_MODE_DIO_WDG,
-		.pin_dir_changeable = FALSE,
-		.pin_mode_changeable = TRUE
-	},
-	.pin[2] = {
-		.pin_id = 5,
-		.pin_dir = PORT_PIN_IN,
-		.pin_mode = PORT_PIN_MODE_CAN,
-		.pin_level = PORT_PIN_LEVEL_LOW,
-		.pin_initial_mode = PORT_PIN_MODE_CAN,
-		.pin_dir_changeable = TRUE,
-		.pin_mode_changeable = TRUE
-	},
-	.pin[3] = {
-		.pin_id = 1,
-		.pin_dir = PORT_PIN_IN,
-		.pin_mode = PORT_PIN_MODE_FLEXRAY,
-		.pin_level = PORT_PIN_LEVEL_LOW,
-		.pin_initial_mode = PORT_PIN_MODE_FLEXRAY,
 		.pin_dir_changeable = FALSE,
 		.pin_mode_changeable = FALSE
 	}
